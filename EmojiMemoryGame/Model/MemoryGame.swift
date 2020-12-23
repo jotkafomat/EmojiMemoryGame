944bc7d2a3e2d06bf0bdd8b4889e59e57123a066
jotkafomat <k-j@xl.wp.pl>
2020-12-23 19:19:42 +0000

    initial commit

---
+++ EmojiMemoryGame/Model/MemoryGame.swift
@@ -0,0 +1,35 @@
+//
+//  MemoryGame.swift
+//  EmojiMemoryGame
+//
+//  Created by Krzysztof Jankowski on 23/12/2020.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    
+    var cards: Array<Card>
+    
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        
+        cards = Array<Card>()
+        
+        for index in 0..<numberOfPairsOfCards {
+            let content = cardContentFactory(index)
+            cards.append(Card(id: index*2, content: content))
+            cards.append(Card(id: index*2+1, content: content))
+        }
+    }
+    
+    func choose(card: Card) {
+        print("card choosen: \(card)")
+    }
+    
+    struct Card: Identifiable {
+        var id: Int
+        var isFaceUp: Bool = true
+        var isMatched: Bool = true
+        var content: CardContent
+    }
+}
---
+++ EmojiMemoryGame/ViewModel/EmojiMemoryGame.swift
@@ -0,0 +1,27 @@
+//
+//  EmojiMemoryGame.swift
+//  EmojiMemoryGame
+//
+//  Created by Krzysztof Jankowski on 23/12/2020.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis: Array<String> = ["😱", "🤬", "💩"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { index in return emojis[index] }
+    }
+    // MARK - Access too the model
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    // MARK: - Intends
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
