406a6203977aa99703978cf00b0e1e2d1f85767f
jotkafomat <k-j@xl.wp.pl>
2020-12-23 14:32:45 +0000

    Initial Commit

---
+++ EmojiMemoryGame/EmojiMemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiMemoryGameApp.swift
+//  EmojiMemoryGame
+//
+//  Created by Krzysztof Jankowski on 23/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct EmojiMemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
