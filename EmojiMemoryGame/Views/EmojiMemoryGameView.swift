20a400f27dd3f25d6a53f8d581c5edbb5c1b9235
jotkafomat <k-j@xl.wp.pl>
2021-01-21 18:08:41 +0000

    add animation to bonus pie time remaining

--- EmojiMemoryGame/Views/EmojiMemoryGameView.swift
+++ EmojiMemoryGame/Views/EmojiMemoryGameView.swift
@@ -11,12 +11,13 @@ struct EmojiMemoryGameView: View {
     
     @ObservedObject var viewModel: EmojiMemoryGame
     
+    
     var body: some View {
         NavigationView {
             HStack {
                 GridView(viewModel.cards) { card in
                     CardView(card: card).onTapGesture {
-                        withAnimation(.linear(duration: 2)) {
+                        withAnimation(.linear(duration: 0.75)) {
                             viewModel.choose(card: card)
                         }
                     }
@@ -51,19 +52,42 @@ struct CardView: View {
             self.body(for: geometry.size)
         }
     }
+    
+    @State private var animatedBonusRemaining: Double = 0
+    
+    private func startBonusTimeAnimation() {
+        animatedBonusRemaining = card.bonusRemaining
+        withAnimation(.linear(duration: card.bonusTimeRemaining)) {
+            animatedBonusRemaining = 0
+        }
+    }
+    
     @ViewBuilder
     private func body(for size: CGSize) -> some View {
         if card.isFaceUp || !card.isMatched {
             ZStack {
-                Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true)
-                    .padding(5)
-                    .opacity(0.4)
+                if card.isConsumingBonusTime {
+                    Pie(startAngle: Angle.degrees(0-90),
+                        endAngle: Angle.degrees(-animatedBonusRemaining*360-90),
+                        clockwise: true)
+                        .padding(5)
+                        .opacity(0.4)
+                        .onAppear {
+                            self.startBonusTimeAnimation()
+                        }
+                } else {
+                    Pie(startAngle: Angle.degrees(0-90),
+                        endAngle: Angle.degrees(-card.bonusRemaining*360-90),
+                        clockwise: true)
+                        .padding(5)
+                        .opacity(0.4)
+                }
                 Text(card.content)
                     .font(Font.system(size: fontSize(for: size)))
                     .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
                     .animation(card.isMatched ?
-                        Animation
-                            .linear(duration: 1.0)
+                                Animation
+                                .linear(duration: 1.0)
                                 .repeatForever(autoreverses: false) : .default)
             }
             .cardify(isFaceUp: card.isFaceUp)
